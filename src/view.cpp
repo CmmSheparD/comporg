a8ac139fecd8b24700631b45e0a55d9c2d1587b9
CmmSheparD <ustynow.gleb@gmail.com>
2022-09-16 22:55:26 +0300

    feat: Added some missing methods to the TypeView interface.

---
+++ src/view.cpp
@@ -0,0 +1,28 @@
+#include "view.hh"
+
+namespace views {
+
+unsigned int TypeView::createFilterMask(size_t start, size_t len)
+{
+	const size_t length = bitlen();
+	unsigned int mask = 1u << length - 1 - start;
+	for (size_t i = 0; i < length - 1; ++i) {
+		unsigned int prev = mask;
+		mask >>= 1u;
+		mask |= prev;
+	}
+	return mask;
+}
+
+std::string TypeView::binaryView()
+{
+	const size_t length = bitlen();
+	std::string view;
+	unsigned int mask = 1u << (sizeof(mask) * 8u - 1u);
+	for (int i = 0; i < length; ++i) {
+		view.push_back(getbit(i) ? '1' : '0');
+	}
+	return view;
+}
+
+}	// namespace views
--- src/view.hh
+++ src/view.hh
@@ -5,21 +5,30 @@
 
 namespace views {
 
-
 class TypeView {
 public:
-	virtual std::string binaryView() = 0;
+	/*
+	 * Created a mask, that is filed with `len` `1`-s starting at
+	 * the `start` position.
+	 */
+	unsigned int createFilterMask(size_t start, size_t len);
+
+	std::string binaryView();
+
 	virtual size_t bitlen() = 0;
 
 	// Bit position is assumed to counted from most significant bit to the least.
 	virtual unsigned int getbit(size_t pos) = 0;
+	// Get mask with filtered out only `n` bits starting from `from` position.
+	virtual unsigned int filter(size_t from, size_t n) = 0;
+
 	/*
-	 * Only the least significant bit of the value is given into account, all
-	 * other bits are ignored.
+	 * Only the least significant bit of the value is taken into
+	 * account, all other bits are ignored.
 	 */
 	virtual void setbits(size_t from, unsigned int value, size_t n = 1) = 0;
+	virtual void setbits_with_mask(size_t from, unsigned int value, size_t len) = 0;
 };
-
 
 }	// namespace views
 
