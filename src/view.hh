878e4cc2d7f82148a33193c1e52a5d8597ba3c11
CmmSheparD <ustynow.gleb@gmail.com>
2022-09-16 11:59:57 +0300

    feat: Let it be a very-very basic typeview.

---
+++ src/view.hh
@@ -0,0 +1,18 @@
+#ifndef VIEW_HH
+#define VIEW_HH
+#include <string>
+
+
+namespace views {
+
+
+class TypeView {
+public:
+	std::string binaryView();
+
+};
+
+
+}	// namespace views
+
+#endif	// VIEW_HH
