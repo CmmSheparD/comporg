4f9876ad0b9eeb4df47157cf44d69213e5464466
CmmSheparD <ustynow.gleb@gmail.com>
2022-09-16 22:59:55 +0300

    feat: Updated UIntView to meet new interface.

--- src/uintview.cpp
+++ src/uintview.cpp
@@ -6,16 +6,6 @@ using namespace std;
 
 namespace views {
 
-std::string UIntView::binaryView()
-{
-	unsigned int mask = 1u << (sizeof(mask) * 8 - 1);
-	std::string view;
-	for (int i = 0; i < bitlen(); ++i) {
-		view.push_back(getbit(i) ? '1' : '0');
-	}
-	return view;
-}
-
 size_t UIntView::bitlen()
 {
 	return sizeof(value_) * 8;
@@ -23,27 +13,40 @@ size_t UIntView::bitlen()
 
 unsigned int UIntView::getbit(size_t pos)
 {
-	const size_t len = bitlen();
-	if (pos >= len)
+	if (pos >= bitlen())
 		throw out_of_range("Trying to get non-existent bit.");
-	return (value_ >> len - 1 - pos) & 1u;
+	return (value_ >> bitlen() - 1u - pos) & 1u;
+}
+
+unsigned int UIntView::filter(size_t from, size_t n)
+{
+	if (from + n >= bitlen())
+		throw out_of_range("Trying to get non-existent bit.");
+	unsigned int filter_mask = createFilterMask(from, n);
+	return value_ & filter_mask;
 }
 
 void UIntView::setbits(size_t from, unsigned int value, size_t n)
 {
-	const size_t len = bitlen();
-	if (from >= len || from + n >= len)
+	if (from >= bitlen() || from + n >= bitlen())
 		throw out_of_range("Trying to get non-existent bit.");
-	unsigned int set_mask = 1u << len - 1 - from;
-	for (size_t i = 0; i < n - 1; ++i) {
-		unsigned int prev = set_mask;
-		set_mask >>= 1u;
-		set_mask |= prev;
-	}
+	unsigned int set_mask = createFilterMask(from, n);
 	if ((value & 1u) == 1u)
 		value_ |= set_mask;
 	else
 		value_ &= ~set_mask;
+}
+
+void UIntView::setbits_with_mask(size_t from, unsigned int mask, size_t len)
+{
+	if (from + len >= bitlen())
+		throw out_of_range("Trying to get non-existent bit.");
+	unsigned int unset_mask = createFilterMask(from, len);
+
+	// ensure that mask has only wanted bits left
+	mask &= unset_mask;
+	value_ &= ~unset_mask;
+	value_ |= mask;
 }
 
 }	// namespace views
--- src/uintview.hh
+++ src/uintview.hh
@@ -5,19 +5,19 @@
 
 #include "view.hh"
 
-
 namespace views {
-
 
 class UIntView : public TypeView {
 public:
 	UIntView(unsigned int value) : value_(value) {}
 
-	std::string binaryView();
-
 	size_t bitlen();
+
 	unsigned int getbit(size_t pos);
+	unsigned int filter(size_t from, size_t n);
+
 	void setbits(size_t from, unsigned int value, size_t n = 1);
+	void setbits_with_mask(size_t from, unsigned int mask, size_t len);
 private:
 	unsigned int value_;	
 };
